d34497d1acaa2873ed52756e14ba4b0c294632ac
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-12-08 23:59:45 +0530

    stopPropagation  vs preventDefault

--- e.stopPropagation-vs-e.preventDefault.js
+++ e.stopPropagation-vs-e.preventDefault.js
@@ -1,1 +1,25 @@
-//
+// StopPropagation vs PreventDefault
+
+// | Feature                      | preventDefault | stopPropagation |
+// | ---------------------------- | ---------------|---------------- |
+// | Stops default browser action | ✅ Yes        | ❌ No           |
+// | Stops event bubbling         | ❌ No         | ✅ Yes          |
+// | Example usage                | Prevent form  |Avoid triggering  |
+//                                |submit, disable| parent click event|
+//                                |link navigation|                 |
+// ------------------------------------------------------------------
+
+// PreventDefault :-
+// 💡 Stops the default browser behavior, but does NOT stop event bubbling / propagation.
+
+// 🟢 Examples:-
+
+// | Action                    | Default Behavior |
+// | ------------------------- | ------------------------- |
+// | Clicking an`<a href="">`  | Navigates to another page |
+// | Submitting a form         | Page reload |
+// | Right - click             | Opens context menu |
+// | Checkbox click            | Toggles check |
+
+
+// StopPropagation :-
